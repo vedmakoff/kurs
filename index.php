d64ef093d30e1ca5f9266653bc0350d72c7a18bb
pmi-php06 <pmi-php06@PMI-vPHP-1.corp.nstu.ru>
2015-03-04 19:53:08 +0600

    Кривой неправильный варинат PHP

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+echo '<!DOCTYPE html>'; 
+
+echo '<html>';
+echo '<head>';
+echo '<meta charset="UTF-8">';
+echo '<title>Мама вышли денег</title>';
+echo '</head>';
+echo '<body>';
+echo '<b>';
+echo 'Hello it\'s php!';
+echo '</b>';        
+echo '    </body>';
+echo '</html>';
