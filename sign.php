285ded8448a55ead5b61198625475867ffec66dc
pmi-php06 <pmi-php06@PMI-vPHP-1.corp.nstu.ru>
2015-03-11 20:39:58 +0600

    регистрационная форма методом GET

--- sign.php
+++ sign.php
@@ -2,9 +2,11 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title><? 
+<title>
+    <? 
        include ("title.php");
-   ?> </title>
+   ?> 
+</title>
 <link rel="stylesheet" href="css/style.css">
 </head>
 
@@ -21,12 +23,12 @@
                 ?>
             </div>
             <div class="content">
-                <form>
-                    <p><label>ФИО: </label><input type="text" /></p>
-                    <p><label>Логин: </label><input type="text" /></p>
-                    <p><label>Пароль: </label><input type="password" /></p>
-                    <p><label>E-mail: </label><input type="text" /></p>
-                    <p><label>Аватар: </label><input type="file" /></p>
+                <form action="sign_handler.php" method="GET">
+                    <p><label>ФИО: </label><input type="text" name="name"/></p>
+                    <p><label>Логин: </label><input type="text" name="login"/></p>
+                    <p><label>Пароль: </label><input type="password" name="pass"/></p>
+                    <p><label>E-mail: </label><input type="text" name="email"/></p>
+                    <p><label>Аватар: </label><input type="file" name="file"/></p>
                     <p><input type="submit" value="Отправить"/></p>
                 </form>
             </div>
---
+++ sign_handler.php
@@ -0,0 +1,5 @@
+<?php
+echo $_GET["name"]."</br>";
+echo $_GET["login"]."</br>";
+echo $_GET["pass"]."</br>";
+echo $_GET["email"]."</br>";
