5281790abf168478c478f704c5fc10320f6b1b96
pmi-php06 <pmi-php06@PMI-vPHP-1.corp.nstu.ru>
2015-03-18 20:06:09 +0600

    Соединение с мускулем

--- sign_handler.php
+++ sign_handler.php
@@ -4,8 +4,30 @@ echo $_POST["login"]."</br>";
 echo $_POST["pass"]."</br>";
 echo $_POST["email"]."</br>";
 
-setcookie("login", $_POST["login"], time()+3600);
-//print_r ($_FILES["file"]);
-echo $_FILES["file"]["tmp_name"]."</br>";
-echo $_FILES["file"]["name"]."</br>";
-move_uploaded_file($_FILES["file"]["tmp_name"], "images/".$_FILES["file"]["name"]);
+$server="localhost";
+$user="root";
+$password="";
+$db="kurs";
+
+$mysqli=mysqli_connect($server, $user, $password, $db);
+if (!$mysqli) {
+echo "Ошибка подключения к серверу MySQL ".  mysqli_connect_error();
+exit;
+}
+$name=$_POST["name"];
+$login=$_POST["login"];
+$pass=$_POST["pass"];
+$email=$_POST["email"];
+
+$result = mysqli_query($mysqli, "INSERT INTO users (name,login,pass,email) VALUES ('$name','$login','$pass','$email')");
+        
+if (!$result) {
+    printf("Error: %s\n", mysqli_error($mysqli));
+}
+mysqli_close($mysqli);
+
+//setcookie("login", $_POST["login"], time()+3600);
+////print_r ($_FILES["file"]);
+//echo $_FILES["file"]["tmp_name"]."</br>";
+//echo $_FILES["file"]["name"]."</br>";
+//move_uploaded_file($_FILES["file"]["tmp_name"], "images/".$_FILES["file"]["name"]);
