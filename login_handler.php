9ef794c966be97cad957892eb85b80868f41cd74
pmi-php06 <pmi-php06@PMI-vPHP-1.corp.nstu.ru>
2015-03-25 19:42:43 +0600

    Сделали таки регистрацию и даже с проверкой на оригинальность логина

--- login_handler.php
+++ login_handler.php
@@ -8,40 +8,38 @@ $password="";
 $db="kurs";
 
 $mysqli=new mysqli($server, $user, $password, $db);
-if (!$mysqli) {
+if ($mysqli==0) {
 echo "Ошибка подключения к серверу MySQL ".  mysqli_connect_error();
 exit;
 }
 $login=$_POST["login"];
 $pass=$_POST["pass"];
 
-$stmt = $mysqli->prepare("SELECT name,email FROM users WHERE login=? and pass=?");
-if ($stmt) {
-
+if ($stmt = $mysqli->prepare("SELECT name,email FROM users WHERE login=? and pass=?")) 
+ {
     /* bind parameters for markers */
     $stmt->bind_param("ss",$login,$pass);
-
-    /* execute query */
-    $stmt->execute();
-    
-if ($stmt->num_rows()!=0)  {
-      
-    /* bind result variables */
-    $stmt->bind_result($name,$email);
-
-    /* fetch value */
-    $stmt->fetch();
-
-    echo "<br> Пользователь".$name." ваша электропочта".$email;
    
-    
+   /* execute query */
+    $stmt->execute();
+	
+	$stmt->store_result();
+	
+	if($stmt->num_rows!=0)
+	{
+		 /* bind result variables */
+		$stmt->bind_result($name,$email);
+		/* fetch value */
+		$stmt->fetch();
+	
+		echo "<br>"."Ваш ФИО: ".$name.", ваш e-mail: ".$email."<br>";
+	}
+	 else
+	 {
+		echo "<br>"."Нет такого пользователя"."<br>";
+	}
+
     /* close statement */
     $stmt->close();
-    
-}
- else {
-    echo '<br>'.'нет такого пользователя'.'<br>';
-}   
 } 
-
-$mysqli->close();
+ $mysqli->close();
