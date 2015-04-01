f326fe76cc080061f088d4e1d5019a77eaa0df45
pmi-php06 <pmi-php06@PMI-vPHP-1.corp.nstu.ru>
2015-04-01 21:38:51 +0600

    Добавили капчу (не работает)

--- captcha.php
+++ captcha.php
@@ -4,16 +4,16 @@ session_start();
 $a=rand(1000,9999);
 $_SESSION["phrase"]=$a;
 
-$im=imagecreatetruecolor(300,100);
+$im=imagecreatetruecolor(100,50);
 
 $red=imagecolorallocate($im, 0xFF, 0x00, 0x00);
 $black=imagecolorallocate($im, 0x00, 0x00, 0x00);
 
-imagefilledrectangle($im, 0, 0, 299, 99, $red);
+imagefilledrectangle($im, 0, 0, 99, 99, $red);
 
 $font_file = "arial.ttf";
 
-imagefttext($im, 20, 5, 105, 55, $black, $font_file, $a);
+imagefttext($im, 20, 5, 10, 50, $black, $font_file, $a);
 
 header("Content-Type: image/png");
 
